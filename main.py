dfe3f374c6375602ff2fc93084265c81e195ae8b
dimitryegorovvv <132154036+dimitryegorovvv@users.noreply.github.com>
2023-11-25 20:36:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
+from background import keep_alive 
 import telebot
 import random
 import time
 from token_file import token
 bot = telebot.TeleBot(token)
 
+its_draw = False
 only_new_game = False
 cards = ['6', '7', '8', '9', '10', 'валет', 'дама', 'король', 'туз']
 convert_cards = {'6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'валет': 2, 'дама': 3, 'король': 4, 'туз': 11}
+
 
 player_cards = [random.choice(cards), random.choice(cards)]
 bot_cards = [random.choice(cards), random.choice(cards)]
@@ -26,21 +29,20 @@ def main(message):
     global bot_cards_sum
     global bot_cards_count
     global only_new_game
+    global its_draw
 
     if (message.text.lower() != 'n' and message.text.lower() != 'y'
             and message.text.lower() != 'h' and message.text.lower() != '/start'):
         bot.send_message(message.chat.id, 'я не знаю такую команду.\nh - новая игра')
 
     if (message.text.lower() == 'n' and only_new_game == False) or len(player_cards) == 5:
-        bot.send_message(message.chat.id, 'очередь бота:')
-        time.sleep(1)
-        bot.send_message(message.chat.id, f'карты бота: {' ; '.join(bot_cards)}')
-        time.sleep(1)
-        bot.send_message(message.chat.id, f'сумма карт бота: {bot_cards_sum}')
+        bot.send_message(message.chat.id, f'очередь бота:\n'
+        f'карты бота: {" ; ".join(bot_cards)}\n'
+        f'сумма карт бота: {bot_cards_sum}')
         time.sleep(1)
 
         while (player_cards_sum >= bot_cards_sum and len(bot_cards) != 5
-                and bot_cards_sum < 22 and its_draw != True):
+                and bot_cards_sum < 22 and its_draw == False):
 
             if ((player_cards_sum == 21 and bot_cards_sum == 21)
                     or (player_cards_sum == 20 and bot_cards_sum == 20)
@@ -48,8 +50,7 @@ def main(message):
                     or (player_cards_sum == 18 and bot_cards_sum == 18)
                     or (player_cards_sum == 17 and bot_cards_sum == 17)
                     or (player_cards_sum == 16 and bot_cards_sum == 16)):
-                bot.send_message(message.chat.id,'ничья')
-                bot.send_message(message.chat.id, 'h - новая игра')
+                bot.send_message(message.chat.id,'ничья\nh - новая игра')
                 its_draw = True
                 only_new_game = True
 
@@ -57,31 +58,25 @@ def main(message):
                 pass
 
             else:
-                bot_cards.append(random.choice(cards))
-                bot.send_message(message.chat.id,f'бот берёт карту: {bot_cards[bot_cards_count]}')
-                time.sleep(1)
-                bot.send_message(message.chat.id, f'карты бота: {' ; '.join(bot_cards)}')
-                time.sleep(1)
-                bot_cards_sum = bot_cards_sum + convert_cards[bot_cards[bot_cards_count]]
-                bot.send_message(message.chat.id, f'сумма карт бота: {bot_cards_sum}')
-                time.sleep(1)
-                bot_cards_count = bot_cards_count + 1
+              bot_cards.append(random.choice(cards))
+              bot_cards_sum = bot_cards_sum + convert_cards[bot_cards[bot_cards_count]]
+              bot.send_message(message.chat.id,f'бот берёт карту: {bot_cards[bot_cards_count]}\n'
+                                               f'карты бота: {" ; ".join(bot_cards)}\n'
+                                               f'сумма карт бота: {bot_cards_sum}')
+              time.sleep(1)
+              bot_cards_count = bot_cards_count + 1
 
         if bot_cards_sum > 21:
-            bot.send_message(message.chat.id,'вы выиграли')
-            bot.send_message(message.chat.id, 'h - новая игра')
+            bot.send_message(message.chat.id,'вы выиграли\nh - новая игра')
             only_new_game = True
         elif bot_cards_sum > player_cards_sum:
-            bot.send_message(message.chat.id,'вы проиграли')
-            bot.send_message(message.chat.id, 'h - новая игра')
+            bot.send_message(message.chat.id,'вы проиграли\nh - новая игра')
             only_new_game = True
         elif bot_cards_sum < player_cards_sum:
-            bot.send_message(message.chat.id, 'вы выиграли')
-            bot.send_message(message.chat.id, 'h - новая игра')
+            bot.send_message(message.chat.id, 'вы выиграли\nh - новая игра')
             only_new_game = True
         elif bot_cards_sum == player_cards_sum:
-            bot.send_message(message.chat.id, 'ничья')
-            bot.send_message(message.chat.id, 'h - новая игра')
+            bot.send_message(message.chat.id, 'ничья\nh - новая игра')
             only_new_game = True
 
     if message.text.lower() == 'h' or message.text.lower() == '/start':
@@ -98,32 +93,26 @@ def main(message):
     if (len(player_cards) != 5 and message.text.lower() != 'y' and message.text.lower() != 'n'
             and only_new_game == False and (message.text.lower() == 'h'
                                             or message.text.lower() == '/start')):
-        bot.send_message(message.chat.id, f'карты бота: {bot_cards[0]} ; ?')
+        bot.send_message(message.chat.id, f'карты бота: {bot_cards[0]} ; ?\n'
+                                          f'ваши карты: {" ; ".join(player_cards)}\n'
+                                          f'сумма ваших карт: {player_cards_sum}\n'
+                                          f'взять карту? (y/n)')
         time.sleep(1)
-        bot.send_message(message.chat.id, f'ваши карты: {' ; '.join(player_cards)}')
-        time.sleep(1)
-        bot.send_message(message.chat.id, f'сумма ваших карт: {player_cards_sum}')
-        time.sleep(1)
-        bot.send_message(message.chat.id, 'взять карту? (y/n) ')
 
     elif message.text.lower() == 'y' and only_new_game == False:
         player_cards_count = player_cards_count + 1
         player_cards.append(random.choice(cards))
         player_cards_sum = player_cards_sum + convert_cards[player_cards[player_cards_count]]
-        bot.send_message(message.chat.id, f'карты бота: {bot_cards[0]} ; ?')
-        time.sleep(1)
-        bot.send_message(message.chat.id, f'вы берёте карту: {player_cards[player_cards_count]}')
-        time.sleep(1)
-        bot.send_message(message.chat.id, f'ваши карты: {' ; '.join(player_cards)}')
-        time.sleep(1)
-        bot.send_message(message.chat.id, f'сумма ваших карт: {player_cards_sum}')
+        bot.send_message(message.chat.id, f'карты бота: {bot_cards[0]} ; ?\n'
+                                          f'вы берёте карту: {player_cards[player_cards_count]}\n'
+                                          f'ваши карты: {" ; ".join(player_cards)}\n'
+                                          f'сумма ваших карт: {player_cards_sum}')
         time.sleep(1)
         if player_cards_sum < 22:
             bot.send_message(message.chat.id, 'взять карту? (y/n) ')
 
     if player_cards_sum > 21 and only_new_game == False:
-        bot.send_message(message.chat.id, 'вы проиграли')
-        bot.send_message(message.chat.id, 'h - новая игра')
+        bot.send_message(message.chat.id, 'вы проиграли\nh - новая игра')
         only_new_game = True
 
 bot.polling(none_stop=True)
